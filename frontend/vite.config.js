190668062172d4e0bd956d8be5dc79f1fdf2ea34
bassem669 <151470122+bassem669@users.noreply.github.com>
2026-05-20 22:32:25 +0100

    Update allowedHosts in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['honest-creativity-production-74db.up.railway.app']
+    allowedHosts: ['gestionassurancemaladie-production.up.railway.app']
   }
 })
